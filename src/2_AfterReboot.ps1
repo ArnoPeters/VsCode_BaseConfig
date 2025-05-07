13f1aa5f96420fbf8630c9d1ffe5b11799ca34eb
Arno Peters <arno.peters@gmail.com>
2025-05-07 15:15:53 +0200

    added generate gpg key script

--- src/2_AfterReboot.ps1
+++ src/2_AfterReboot.ps1
@@ -6,6 +6,25 @@ param(
   [string]$GitEmail
 )
 
-git config --global user.name $GitUsername
+
+# Configure Git user details
+git config --global user.name $GitUserName
 git config --global user.email $GitEmail
 
+# Generate a GPG key
+gpg --batch --generate-key <<EOF
+Key-Type: default
+Key-Length: 2048
+Subkey-Type: default
+Name-Real: $GitUserName
+Name-Email: $GitEmail
+Expire-Date: 0
+%no-protection
+EOF
+
+# Extract the GPG key ID
+$GpgKeyId = gpg --list-secret-keys --keyid-format LONG $GitEmail | Select-String -Pattern "sec.*\/([A-F0-9]+)" | ForEach-Object { $_.Matches.Groups[1].Value }
+
+# Configure Git to use the GPG key
+git config --global user.signingkey $GpgKeyId
+git config --global commit.gpgSign true
