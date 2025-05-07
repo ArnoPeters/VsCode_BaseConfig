660e0352419731b44b44e12ca95a546e8729230e
Arno Peters <arno.peters@gmail.com>
2025-05-07 15:34:11 +0200

    Added condition to gpg key

--- src/Helpers/GitConfigureRepo.ps1
+++ src/Helpers/GitConfigureRepo.ps1
@@ -13,19 +13,17 @@ param(
 git config user.name $GitUserName
 git config user.email $GitEmail
 
-
 if ($UseGPG -eq $true) {
 
-  # Extract the GPG key ID
+  # Check if a GPG key already exists for the provided email
   $GpgKeyId = gpg --list-secret-keys --keyid-format LONG $GitEmail | Select-String -Pattern "sec.*\/([A-F0-9]+)" | ForEach-Object { $_.Matches.Groups[1].Value }
 
-  #if no key, generate one and try again
+  if (-not $GpgKeyId) {
+    Write-Host "No GPG key found for $GitEmail. Generating a new key..."
 
-
-  # Generate a GPG key
-  # Create a temporary file for the GPG batch input
-  $GpgBatchFile = New-TemporaryFile
-  @"
+    # Create a temporary file for the GPG batch input
+    $GpgBatchFile = New-TemporaryFile
+    @"
 Key-Type: default
 Key-Length: 2048
 Subkey-Type: default
@@ -35,14 +33,18 @@ Expire-Date: 0
 %no-protection
 "@ | Set-Content -Path $GpgBatchFile
 
-  # Generate a GPG key using the batch file
-  gpg --batch --generate-key $GpgBatchFile
+    # Generate a GPG key using the batch file
+    gpg --batch --generate-key $GpgBatchFile
 
-  # Remove the temporary file
-  Remove-Item -Path $GpgBatchFile -Force
+    # Remove the temporary file
+    Remove-Item -Path $GpgBatchFile -Force
 
-  # Extract the GPG key ID
-  $GpgKeyId = gpg --list-secret-keys --keyid-format LONG $GitEmail | Select-String -Pattern "sec.*\/([A-F0-9]+)" | ForEach-Object { $_.Matches.Groups[1].Value }
+    # Extract the newly generated GPG key ID
+    $GpgKeyId = gpg --list-secret-keys --keyid-format LONG $GitEmail | Select-String -Pattern "sec.*\/([A-F0-9]+)" | ForEach-Object { $_.Matches.Groups[1].Value }
+  }
+  else {
+    Write-Host "GPG key already exists for $GitEmail : $GpgKeyId"
+  }
 
   # Configure Git to use the GPG key
   git config user.signingkey $GpgKeyId
