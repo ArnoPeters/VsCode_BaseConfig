38fafbe644f6b3b1f5a539f53c99c233cf3e4f24
Arno Peters <arno.peters@gmail.com>
2025-05-07 17:20:34 +0200

    Install updates

--- src/!Getting_Started/2_Microsoft_Azure.ps1
+++ src/!Getting_Started/2_Microsoft_Azure.ps1
@@ -1,5 +1,8 @@
+wsl --install
+
 winget install -e --source winget --id Microsoft.PowerShell
 winget install -e --source winget --id Microsoft.AzureCLI 
 
-
+#todo az aks install-cli
+# Throws error if cert is not trusted.  
 #todo: az bicep install 
