2cf55cf1304372c6840f0eae58806d5549faa0b6
Arno Peters <arno.peters@gmail.com>
2025-05-07 13:54:25 +0200

    Fix to after reboot

--- src/2_AfterReboot.ps1
+++ src/2_AfterReboot.ps1
@@ -1,3 +1,11 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$GitUserName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$GitEmail
+)
+
 git config --global user.name $GitUsername
 git config --global user.email $GitEmail
 
